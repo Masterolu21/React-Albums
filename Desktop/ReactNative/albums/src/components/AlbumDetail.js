74cff6694803c2022e19578a98f8f967757d8dd8
David Olu-Talabi <masterolu@Davids-MacBook-Air.local>
2018-06-25 18:13:11 -0400

    Made album titles larger andcentered

--- Desktop/ReactNative/albums/src/components/AlbumDetail.js
+++ Desktop/ReactNative/albums/src/components/AlbumDetail.js
@@ -8,14 +8,14 @@ const AlbumDetail = ({ album }) => {
   return (
     <Card>
       <CardSection>
-        <View>
+        <View style={styles.thumbnailContainerStyle}>
           <Image
           style={styles.thumbnailStyle}
           source={{ uri: thumbnail_image }}
           />
         </View>
         <View style={styles.headerContentStyle}>
-          <Text>{title}</Text>
+          <Text style={styles.headerTextstyle}>{title}</Text>
           <Text>{artist}</Text>
         </View>
       </CardSection>
@@ -27,9 +27,19 @@ headerContentStyle: {
   flexDirection: 'column',
   justifyContent: 'space-around'
 },
+  headerTextstyle: {
+    fontSize: 20
+  },
   thumbnailStyle: {
     height: 50,
     width: 50
+  },
+
+  thumbnailContainerStyle: {
+    justifyContent: 'center',
+    alignItems: 'center',
+    marginLeft: 10,
+    marginRight: 10
   }
 };
 
