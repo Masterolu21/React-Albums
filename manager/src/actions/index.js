960a4e00188d3189213b72bf935172091653c4db
David Olu-Talabi <masterolu@Davids-MacBook-Air.local>
2018-08-18 14:53:48 -0400

    Authentication fixed

--- manager/src/actions/index.js
+++ manager/src/actions/index.js
@@ -35,12 +35,12 @@ export const loginUser = ({ email, password }) => {
   };
 };
 const loginUserFail = (dispatch) => {
-  dispatch({type: LOGIN_USER_FAIL});
+  dispatch({ type: LOGIN_USER_FAIL });
 };
 
 const loginUserSuccess = (dispatch, user) => {
   dispatch({
     type: LOGIN_USER_SUCCESS,
-    payload:User
+    payload: user
   });
 };
